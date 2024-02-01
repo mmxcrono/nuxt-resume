d12e41ab326168c611be44818c3565c5a2d58d2e
Hoang <hnguyen.email@gmail.com>
2024-01-31 21:37:15 -0800

    Add centered wrapper

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,10 @@
   <div
     class="min-h-screen bg-gradient-to-t from-teal-900 to-indigo-900 text-zinc-100"
   >
-    <AppHeader />
-    <slot></slot>
-    <AppFooter />
+    <div class="mx-auto max-w-5xl">
+      <AppHeader />
+      <slot></slot>
+      <AppFooter />
+    </div>
   </div>
 </template>
